d09cbfef0b5cac42f0f1ac210cf4a1c44cd1bff0
ivannew <ivanenriquesotorodriguez@gmail.com>
2023-09-29 10:52:21 -0700

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using EXAMEN_IESR.metodos;
+using Microsoft.VisualBasic;
+
+namespace EXAMEN_IESR
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Intervalo
+             Console.Write("Introduce el primer número del intervalo: ");
+             int inicio = int.Parse(Console.ReadLine());
+
+             Console.Write("Introduce el segundo número del intervalo: ");
+             int final = int.Parse(Console.ReadLine());
+
+             Tabla tabla = new Tabla();
+             tabla.CalcularIntervalo(inicio, final);
+
+            //Calculadora
+              Console.WriteLine("ingrese el numero que quiere multiplicar");
+              int numero = Convert.ToInt32(Console.ReadLine());
+
+              Tabla tabla1 = new Tabla();
+              tabla1.CalcularTabla(numero, 1);
+
+            //El de la curp
+            Console.WriteLine("ingrese su curp(18 digitos)");
+            string curp = Console.ReadLine();
+            
+
+
+
+        }
+    }
+}
---
+++ metodos/Tabla.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXAMEN_IESR.metodos
+{
+    internal class Tabla
+    {
+        public void CalcularIntervalo(int inicio, int final)
+        {
+
+            if (inicio > final)
+            {
+                Console.WriteLine("El número de inicio debe ser menor o igual al número final.");
+            }
+            else
+            {
+
+                for (int numero = inicio; numero <= final; numero++)
+                {
+                    Console.WriteLine(numero);
+                }
+            }
+        }
+
+        public void CalcularTabla(int numero, int multiplicador)
+        {
+            if (multiplicador <= 10)
+            {
+                int resultado = numero * multiplicador;
+
+                Console.WriteLine($"{numero}x{multiplicador}={resultado}");
+
+                CalcularTabla(numero, ++multiplicador);
+            }
+           
+        }
+
+
+        public void CalcularDatos(string fecha, string datos, int digitos)
+        {
+            for(int i = 0; i < datos.Length; i++)
+            {
+                if(digitos>18)
+                {
+                    Console.WriteLine("lo siento son demaciados digitos");
+
+                }
+                else if (digitos > 18)
+                {
+                    Console.WriteLine("lo siento faltan digitos");
+                }
+                else
+                {
+
+                }
+
+
+
+            }
+           
+        }
+
+
+
+
+    }
+}
